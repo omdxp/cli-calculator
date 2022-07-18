1f3ec97401de8af6f8324c8f7ed588e9b23e4cc7
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-07-18 22:01:37 +0100

    parse args and operate function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env::{args, Args};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Args = args();
+
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
+    let second = args.nth(0).unwrap();
+
+    let first_number = first.parse::<f32>().unwrap();
+    let second_number = second.parse::<f32>().unwrap();
+    let result = operate(operator, first_number, second_number);
+
+    println!("{}", result);
+}
+
+fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
+    if operator == '+' {
+        first_number + second_number
+    } else if operator == '-' {
+        first_number - second_number
+    } else if operator == '/' {
+        first_number / second_number
+    } else if operator == '*' {
+        first_number * second_number
+    } else {
+        0.0
+    }
 }
