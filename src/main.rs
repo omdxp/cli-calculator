94b973721454dfb744fac65c88a413f7faa941a3
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-07-18 22:21:53 +0100

    match multiple multiply operator

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
     match operator {
         '+' => first_number + second_number,
         '-' => first_number - second_number,
-        '*' => first_number * second_number,
+        '*' | 'x' | 'X' => first_number * second_number,
         '/' => first_number / second_number,
         _ => panic!("Invalid operator"),
     }
