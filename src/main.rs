51277cb8fd2d768b20e24523c9772f5fcf9262d9
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-07-18 22:05:40 +0100

    add format output function

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,10 @@ fn main() {
     let second_number = second.parse::<f32>().unwrap();
     let result = operate(operator, first_number, second_number);
 
-    println!("{}", result);
+    println!(
+        "{:?}",
+        output(first_number, operator, second_number, result)
+    );
 }
 
 fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
@@ -26,4 +29,11 @@ fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
     } else {
         0.0
     }
+}
+
+fn output(first_number: f32, operator: char, second_number: f32, result: f32) -> String {
+    format!(
+        "{} {} {} = {}",
+        first_number, operator, second_number, result
+    )
 }
